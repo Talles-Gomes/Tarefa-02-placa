43336228ea3ccbb24aae0299cb1bb11ac731f475
Talles Gomes <tallespk@gmail.com>
2025-01-17 11:07:38 -0300

    Instruções da tarefa 02

---
+++ Tarefa02.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+const uint Buzzer = 10;
+const uint led_G = 11;
+const uint led_B = 12;
+const uint led_R = 13;
+#define Tempo_led = 500;
+
+//acender e apagar led verde
+
+
+
+//acender e apagar led vermelho
+
+
+
+//acender e apagar led azul
+
+
+
+//acender e apagar todos os leds
+
+
+
+//ativar o buzzer por 2 segundos
+
+
+
+int main(){
+    //inicialização das portas com os pinos e o buzzer
+
+
+  
+
+while(true){
+ 
+ // rotina dos leds e do buzzer
+
+
+}
+
+
+}
