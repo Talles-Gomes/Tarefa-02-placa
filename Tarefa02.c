6c98e4873385d37864d497bc9e4dc95366673eb4
Talles Gomes <tallespk@gmail.com>
2025-01-19 21:06:09 -0300

    versão final da tarefa 02

--- Tarefa02.c
+++ Tarefa02.c
@@ -2,15 +2,13 @@
 #include "pico/stdlib.h"
 #include "pico/bootrom.h"
 
-
 const uint Buzzer = 21;
 const uint led_G = 11;
 const uint led_B = 12;
 const uint led_R = 13;
-#define Tempo_led 500
+#define Tempo_led 1000
 
 //acender e apagar led verde
-
 void acionar_led_verde() { 
     gpio_put(led_R, 0);
     gpio_put(led_B, 0);
@@ -18,9 +16,7 @@ void acionar_led_verde() {
     sleep_ms(Tempo_led);
     gpio_put(led_G, 0);
 }
-
 //acender e apagar led vermelho
-
 void acionar_led_vermelho() { 
     gpio_put(led_R, 1);
     gpio_put(led_B, 0);
@@ -28,9 +24,7 @@ void acionar_led_vermelho() {
     sleep_ms(Tempo_led);
     gpio_put(led_R, 0);
 }
-
 //acender e apagar led azul
-
 void acionar_led_azul() { 
     gpio_put(led_R, 0);
     gpio_put(led_B, 1);
@@ -38,13 +32,19 @@ void acionar_led_azul() {
     sleep_ms(Tempo_led);
     gpio_put(led_B, 0);
 }
-
 //acender e apagar todos os leds
-
-
-
+void acionar_todos() { 
+    gpio_put(led_R, 1);
+    gpio_put(led_B, 1);
+    gpio_put(led_G, 1);
+    sleep_ms(Tempo_led);
+    gpio_put(led_R, 0);
+    gpio_put(led_B, 0);
+    gpio_put(led_G, 0);
+}
 //ativar o buzzer por 2 segundos
-void acionar_buzzer(){
+void acionar_buzzer()
+{
     // tempo de acionamento do buzzer
     int time =2000;
     // executa enquanto o contador não zeroa
@@ -62,12 +62,12 @@ void acionar_buzzer(){
         // decrementa contador
         time-=3;
     }
-
-    // inicializa bootsel
-    void boot_mode(){
-        printf("Entrando em modo bootsel em 5s!!\n");
+}
+// inicializa bootsel
+void boot_mode(){
+        printf("Entrando em modo bootsel em 3s!!\n");
         //pisca led vermelho
-        for( int i=0;i<5;i++){
+        for( int i=0;i<3;i++){
             gpio_put(led_R, 1);
             sleep_ms(Tempo_led);
             gpio_put(led_R, 0);
@@ -78,16 +78,25 @@ void acionar_buzzer(){
 
 int main(){
     //inicialização das portas com os pinos e o buzzer
-
-
-  
+stdio_init_all();  
+ gpio_init(led_G);
+ gpio_set_dir(led_G, GPIO_OUT);
+ gpio_init(led_B);
+ gpio_set_dir(led_B, GPIO_OUT);
+ gpio_init(led_R);
+ gpio_set_dir(led_R, GPIO_OUT);
+ gpio_init(Buzzer);
+ gpio_set_dir(Buzzer, GPIO_OUT);
 
 while(true){
  
  // rotina dos leds e do buzzer
-
-
+acionar_led_verde();
+acionar_led_azul();
+acionar_led_vermelho();
+acionar_todos();
+acionar_buzzer();
+boot_mode();
 }
-
 
 }
